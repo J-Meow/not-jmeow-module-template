2431d165db537fadf768b918a61950c07f5eb8f2
Jmeow <j@jmeow.net>
2025-11-16 12:54:03 -0600

    Secure websocket

--- main.ts
+++ main.ts
@@ -26,7 +26,7 @@ const keyPair = {
 const textDecoder = new TextDecoder()
 
 const socket = new WebSocket(
-    "ws://" +
+    "wss://" +
         Deno.env.get("SERVER_HOST") +
         ":7531/connect?module=moduleTemplate&secret=" +
         encodeURIComponent(Deno.env.get("CONNECTION_SECRET")!),
